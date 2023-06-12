05c86c6f5bcb22db58e5412900d83affb76ef15f
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-06-11 22:34:49 -0700

    Add files via upload

---
+++ HackerNewss.js
@@ -0,0 +1,436 @@
+"use strict";
+
+// So we don't have to keep re-finding things on page, find DOM elements once:
+
+const $body = $("body");
+
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+
+/** To make it easier for individual components to show just themselves, this
+ * is a useful function that hides pretty much everything on the page. After
+ * calling this, individual components can re-show just what they want.
+ */
+
+function hidePageComponents() {
+  const components = [
+    $allStoriesList,
+    $loginForm,
+    $signupForm,
+  ];
+  components.forEach(c => c.hide());
+}
+
+/** Overall function to kick off the app. */
+
+async function start() {
+  console.debug("start");
+
+  // "Remember logged-in user" and log in, if credentials in localStorage
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+
+  // if we got a logged-in user
+  if (currentUser) updateUIOnUserLogin();
+}
+
+// Once the DOM is entirely loaded, begin the app
+$(start);
+"use strict";
+
+const BASE_URL = "https://hack-or-snooze-v3.herokuapp.com";
+
+/******************************************************************************
+ * Story: a single story in the system
+ */
+
+class Story {
+
+  /** Make instance of Story from data object about story:
+   *   - {title, author, url, username, storyId, createdAt}
+   */
+
+  constructor({ storyId, title, author, url, username, createdAt }) {
+    this.storyId = storyId;
+    this.title = title;
+    this.author = author;
+    this.url = url;
+    this.username = username;
+    this.createdAt = createdAt;
+  }
+
+  /** Parses hostname out of URL and returns it. */
+
+  getHostName() {
+    return "hostname.com";
+  }
+}
+
+
+/******************************************************************************
+ * List of Story instances: used by UI to show story lists in DOM.
+ */
+
+class StoryList {
+  constructor(stories) {
+    this.stories = stories;
+  }
+
+  /** Generate a new StoryList. It:
+   *
+   *  - calls the API
+   *  - builds an array of Story instances
+   *  - makes a single StoryList instance out of that
+   *  - returns the StoryList instance.
+   */
+
+  static async getStories() {
+    const response = await axios({
+      url: `${BASE_URL}/stories`,
+      method: "GET",
+    });
+
+    // turn plain old story objects from API into instances of Story class
+    const stories = response.data.stories.map(story => new Story(story));
+
+    // build an instance of our own class using the new array of stories
+    return new StoryList(stories);
+  }
+
+  /** Adds story data to API, makes a Story instance, adds it to story list.
+   * - user - the current instance of User who will post the story
+   * - obj of {title, author, url}
+   * Returns the new Story instance
+   */
+
+  async addStory(user, newstory) {
+    newstory.append($allStoriesList)
+  }
+}
+
+
+/******************************************************************************
+ * User: a user in the system (only used to represent the current user)
+ */
+
+class User {
+  /** Make user instance from obj of user data and a token:
+   *   - {username, name, createdAt, favorites[], ownStories[]}
+   *   - token
+   */
+
+  constructor({
+                username,
+                name,
+                createdAt,
+                favorites = [],
+                ownStories = []
+              },
+              token) {
+    this.username = username;
+    this.name = name;
+    this.createdAt = createdAt;
+
+    // instantiate Story instances for the user's favorites and ownStories
+    this.favorites = favorites.map(s => new Story(s));
+    this.ownStories = ownStories.map(s => new Story(s));
+
+    // store the login token on the user so it's easy to find for API calls.
+    this.loginToken = token;
+  }
+
+  /** Register new user in API, make User instance & return it.
+   *
+   * - username: a new username
+   * - password: a new password
+   * - name: the user's full name
+   */
+
+  static async signup(username, password, name) {
+    const response = await axios({
+      url: `${BASE_URL}/signup`,
+      method: "POST",
+      data: { user: { username, password, name } },
+    });
+
+    let { user } = response.data
+
+    return new User(
+      {
+        username: user.username,
+        name: user.name,
+        createdAt: user.createdAt,
+        favorites: user.favorites,
+        ownStories: user.stories
+      },
+      response.data.token
+    );
+  }
+
+  /** Login in user with API, make User instance & return it.
+
+   * - username: an existing user's username
+   * - password: an existing user's password
+   */
+
+  static async login(username, password) {
+    const response = await axios({
+      url: `${BASE_URL}/login`,
+      method: "POST",
+      data: { user: { username, password } },
+    });
+
+    let { user } = response.data;
+
+    return new User(
+      {
+        username: user.username,
+        name: user.name,
+        createdAt: user.createdAt,
+        favorites: user.favorites,
+        ownStories: user.stories
+      },
+      response.data.token
+    );
+  }
+
+  /** When we already have credentials (token & username) for a user,
+   *   we can log them in automatically. This function does that.
+   */
+
+  static async loginViaStoredCredentials(token, username) {
+    try {
+      const response = await axios({
+        url: `${BASE_URL}/users/${username}`,
+        method: "GET",
+        params: { token },
+      });
+
+      let { user } = response.data;
+
+      return new User(
+        {
+          username: user.username,
+          name: user.name,
+          createdAt: user.createdAt,
+          favorites: user.favorites,
+          ownStories: user.stories
+        },
+        token
+      );
+    } catch (err) {
+      console.error("loginViaStoredCredentials failed", err);
+      return null;
+    }
+  }
+}
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+  console.log('ayo bada bing bada boom');
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+"use strict";
+
+// global to hold the User instance of the currently-logged-in user
+let currentUser;
+
+/******************************************************************************
+ * User login/signup/login
+ */
+
+/** Handle login form submission. If login ok, sets up the user instance */
+
+async function login(evt) {
+  console.debug("login", evt);
+  evt.preventDefault();
+
+  // grab the username and password
+  const username = $("#login-username").val();
+  const password = $("#login-password").val();
+
+  // User.login retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.login(username, password);
+
+  $loginForm.trigger("reset");
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+}
+
+$loginForm.on("submit", login);
+
+/** Handle signup form submission. */
+
+async function signup(evt) {
+  console.debug("signup", evt);
+  evt.preventDefault();
+
+  const name = $("#signup-name").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  // User.signup retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.signup(username, password, name);
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+
+  $signupForm.trigger("reset");
+}
+
+$signupForm.on("submit", signup);
+
+/** Handle click of logout button
+ *
+ * Remove their credentials from localStorage and refresh page
+ */
+
+function logout(evt) {
+  console.debug("logout", evt);
+  localStorage.clear();
+  location.reload();
+}
+
+$navLogOut.on("click", logout);
+
+/******************************************************************************
+ * Storing/recalling previously-logged-in-user with localStorage
+ */
+
+/** If there are user credentials in local storage, use those to log in
+ * that user. This is meant to be called on page load, just once.
+ */
+
+async function checkForRememberedUser() {
+  console.debug("checkForRememberedUser");
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+  if (!token || !username) return false;
+
+  // try to log in with these credentials (will be null if login failed)
+  currentUser = await User.loginViaStoredCredentials(token, username);
+}
+
+/** Sync current user information to localStorage.
+ *
+ * We store the username/token in localStorage so when the page is refreshed
+ * (or the user revisits the site later), they will still be logged in.
+ */
+
+function saveUserCredentialsInLocalStorage() {
+  console.debug("saveUserCredentialsInLocalStorage");
+  if (currentUser) {
+    localStorage.setItem("token", currentUser.loginToken);
+    localStorage.setItem("username", currentUser.username);
+  }
+}
+
+/******************************************************************************
+ * General UI stuff about users
+ */
+
+/** When a user signs up or registers, we want to set up the UI for them:
+ *
+ * - show the stories list
+ * - update nav bar options for logged-in user
+ * - generate the user profile part of the page
+ */
+
+function updateUIOnUserLogin() {
+  console.debug("updateUIOnUserLogin");
+  $allStoriesList.show();
+  updateNavOnLogin();
+}
